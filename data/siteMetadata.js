f8ffdeee964b042560415d39cedd2814f9fc766d
ambirpatel <ambir.patel@gmail.com>
2021-07-27 13:26:52 +0530

    updating images and configs

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,36 +5,34 @@ const siteMetadata = {
   description: 'Blogs about data science and programming',
   language: 'en-us',
   siteUrl: 'https://ambirpatel.me',
-  siteRepo: 'https://github.com/ambirpatel/ambirpatel.github.io',
+  siteRepo: 'https://github.com/ambirpatel/portfolio',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'ambir.patel@gmail.com',
   github: 'https://github.com/ambirpatel',
   twitter: 'https://twitter.com/ambir_p',
-  // facebook: 'https://facebook.com',
-  // youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com/in/ambirpatel',
   locale: 'en-US',
   comment: {
-    provider: 'giscus', // supported providers: giscus, utterances, disqus
+    provider: '', // supported providers: giscus, utterances, disqus
     giscusConfig: {
-      repo: 'timlrx/tailwind-nextjs-starter-blog', // username/repoName
+      repo: '', // username/repoName
       // Visit the link below, enter your repo in the configuration section and copy the script data parameters
       // Before that you should create a new Github discussions category with the Announcements type so that new discussions can only be created by maintainers and giscus
       // https://giscus.app/
-      repositoryId: 'MDEwOlJlcG9zaXRvcnkzMjgxMjEyNjA=',
-      category: 'Blog Comments',
-      categoryId: 'DIC_kwDOE467rM4B-XTQ',
-      mapping: 'pathname', // supported options: pathname, url, title
-      reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
+      repositoryId: '',
+      category: '',
+      categoryId: '',
+      mapping: '', // supported options: pathname, url, title
+      reactions: '', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
-      metadata: '0',
+      metadata: '',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: '',
       // theme when dark mode
-      darkTheme: 'transparent_dark',
+      darkTheme: '',
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
